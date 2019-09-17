6e2ccf4717c25a0a9c05742a35db01a7c407d273
Reinier van Leussen <rvanleussen@outlook.com>
2019-09-17 21:48:06 +0200

    Responding to settings change without reload

--- src/extension.ts
+++ src/extension.ts
@@ -3,16 +3,27 @@ import { ServantProvider } from './servants';
 import { resolve } from 'path';
 import { existsSync } from 'fs';
 
-export function activate(context: vscode.ExtensionContext) {
+function setEmpressView() {
 	let configuration = vscode.workspace.getConfiguration('empress');
-	if(configuration.has('openlibertyBaseDirectory')) {
-		let baseDir:string | undefined = configuration.get('openlibertyBaseDirectory');
-		if(baseDir !== undefined && existsSync(resolve(baseDir, './bin'))) {
+	let baseDir:string | undefined = configuration.get('openlibertyBaseDirectory');
+	if(baseDir !== undefined && baseDir !== '') {
+		if(existsSync(resolve(baseDir, './bin'))) {
 			vscode.window.registerTreeDataProvider("empressView", new ServantProvider(baseDir));
+		} else {
+			vscode.window.showInformationMessage(`Could not find a Liberty installation at: ${baseDir}`);
 		}
 	} else {
 		vscode.window.showInformationMessage('Please set the value "openlibertyBaseDirectory" for the Empress extension to work');
 	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	let conf = vscode.workspace.onDidChangeConfiguration((e) => {
+		if(e.affectsConfiguration('empress.openlibertyBaseDirectory')) {
+			setEmpressView();
+		}
+	});
+	setEmpressView();
 }
 
 export function deactivate() {}
